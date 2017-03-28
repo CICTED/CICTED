64a50472b6db80fafcd3ec0c33bf5f763fb63f3e
Bruna Silva Campos <bruna.sc1997@hotmail.com>
2017-03-28 14:28:11 -0300

    Add ApplicationDbContext

---
+++ CICTED/src/CICTED.Domain/Infrastucture/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace CICTED.Domain.Infrastucture.Contexts
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<long>, long>        
+    {
+    }
+}
