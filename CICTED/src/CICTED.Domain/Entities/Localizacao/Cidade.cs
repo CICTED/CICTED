7bc30702bc7a82889cda19da7a8b5e0889ef4389
Bruna Silva Campos <bruna.sc1997@hotmail.com>
2017-03-28 18:26:12 -0300

    Cidade

---
+++ CICTED/src/CICTED.Domain/Entities/Localizacao/Cidade.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CICTED.Domain.Entities.Localizacao
+{
+    public class Cidade
+    {
+        [Key]
+        public long Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public int EstadoId { get; set; }
+    }
+}
