b02cb2c2aeb71d2a53842e4dcf1bc9f803fc7f8a
Bruna Silva Campos <bruna.sc1997@hotmail.com>
2017-03-28 17:20:07 -0300

    ApplicationUser

--- CICTED/src/CICTED.Domain/Entities/Account/ApplicationUser.cs
+++ CICTED/src/CICTED.Domain/Entities/Account/ApplicationUser.cs
@@ -8,6 +8,16 @@ namespace CICTED.Domain.Entities.Account
 {
     public class ApplicationUser : IdentityUser<long>
     {
-
+        public string EmailSecundario { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public string CPF { get; set; }
+        public string Documento { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public bool Genero { get; set; }
+        public string Celular { get; set; }
+        public bool Estudante { get; set; }
+        public bool Bolsista { get; set; }
     }
 }
