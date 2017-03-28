98b4fde58e12175c40b6adfc437d8861cf15f87d
Bruna Silva Campos <bruna.sc1997@hotmail.com>
2017-03-28 18:05:07 -0300

    Instituicao StatusUsuario

---
+++ CICTED/src/CICTED.Domain/Entities/Instituicao/Instituicao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CICTED.Domain.Entities.Instituicao
+{
+    public class Instituicao
+    {
+        [Key]
+        public long Id { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
