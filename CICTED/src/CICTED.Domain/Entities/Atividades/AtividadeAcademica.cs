39eafbbe9151229637870cda6f8649ec17f29c9f
lamnaldi <laramenaldi@gmail.com>
2017-03-29 14:02:32 -0300

    Atividade

--- CICTED/src/CICTED.Domain/Entities/Atividades/AtividadeAcademica.cs
+++ CICTED/src/CICTED.Domain/Entities/Atividades/AtividadeAcademica.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CICTED.Domain.Entities.Account;
+using CICTED.Domain.Entities.Trabalho;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,32 @@ namespace CICTED.Domain.Entities.Atividades
 {
     public class AtividadeAcademica
     {
+        public long TipoAtividadeId { get; set; }
+
+        public long StatusTrabalhoId { get; set; }
+
+        public long UserId { get; set; }
+
+        public string Departamento { get; set; }
+
+        public DateTime DataApresentacao { get; set; }
+
+        public string Identificacao { get; set; }
+
+        public int QtdPublico { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string Resumo { get; set; }
+
+        public string Observacao { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public TipoAtividades TipoAtividade { get; set; }
+
+        public StatusTrabalho StatusTrabalho { get; set; }
+
+        public ApplicationUser User { get; set; }
     }
 }
---
+++ CICTED/src/CICTED.Domain/Entities/Atividades/RecursosAtividadeAcademica.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CICTED.Domain.Entities.Atividades
+{
+    public class RecursosAtividadeAcademica
+    {
+        public long RecursosId { get; set; }
+
+        public long AtividadeAcademicaId { get; set; }
+
+        public Recursos Recursos { get; set; }
+
+        public AtividadeAcademica AtividadeAcademica { get; set; }
+    }
+}
