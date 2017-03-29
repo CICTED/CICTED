99e98e74374f265600a73179e182c452ceec1628
lamnaldi <laramenaldi@gmail.com>
2017-03-29 13:32:31 -0300

    Avaliador

---
+++ CICTED/src/CICTED.Domain/Entities/Avaliador/AvaliadorSubAreaConhecimento.cs
@@ -0,0 +1,20 @@
+﻿using CICTED.Domain.Entities.Account;
+using CICTED.Domain.Entities.Trabalho;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CICTED.Domain.Entities.Avaliador
+{
+    public class AvaliadorSubAreaConhecimento
+    {
+        public long UserId { get; set; }
+
+        public long SubAreaConhecimentoId { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+        public SubAreaConhecimento AreaConhecimento { get; set; }
+    }
+}
