bd5a8296aad3dc2188e7e79fc6d0a7e6e677bf94
LuizGustavoPereira <pereiraguus@gmail.com>
2017-03-29 14:11:53 -0300

    ...

---
+++ CICTED/src/CICTED.Domain/Entities/Avaliador/PrioridadeAvaliador.cs
@@ -0,0 +1,21 @@
+﻿using CICTED.Domain.Entities.Account;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CICTED.Domain.Entities.Avaliador
+{
+    public class PrioridadeAvaliador
+    {
+        [Required]
+        public long Id { get; set; }
+        [Required]
+        public long UserId { get; set; }
+        [Required]
+        public int Prioridade { get; set; }
+
+        public ApplicationUser UsuarioId { get; set; }
+    }
+}
