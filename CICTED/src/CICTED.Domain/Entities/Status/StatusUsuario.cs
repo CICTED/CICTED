738b0636f5c755ae232105cabb551a844a623dd7
Bruna Silva Campos <bruna.sc1997@hotmail.com>
2017-03-29 14:03:18 -0300

    Revert "Endereco, StatusUsuario"
    This reverts commit c7879fd5859e0e76f9e43bf1d3076fe077dc2d27.

--- CICTED/src/CICTED.Domain/Entities/Status/StatusUsuario.cs
+++ CICTED/src/CICTED.Domain/Entities/Status/StatusUsuario.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +7,5 @@ namespace CICTED.Domain.Entities.Status
 {
     public class StatusUsuario
     {
-        [Key]
-        public long Id { get; set; }
-
-        public string Nome { get; set; }
     }
 }
