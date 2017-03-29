590d022599f948b07a125dd273f457adf352caaa
Bruna Silva Campos <bruna.sc1997@hotmail.com>
2017-03-29 14:05:21 -0300

    StatusUsuario

--- CICTED/src/CICTED.Domain/Entities/Status/StatusUsuario.cs
+++ CICTED/src/CICTED.Domain/Entities/Status/StatusUsuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,9 @@ namespace CICTED.Domain.Entities.Status
 {
     public class StatusUsuario
     {
+        [Key]
+        public long Id { get; set; }
+
+        public string Nome { get; set; }
     }
 }
