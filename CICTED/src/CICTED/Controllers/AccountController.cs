45e39d181b67277fe60abaa9033a5c82e38a4152
LuizGustavoPereira <pereiraguus@gmail.com>
2017-04-04 16:08:25 -0300

    ...

---
+++ CICTED/src/CICTED/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using CICTED.Domain.Entities.Account;
+using Microsoft.AspNetCore.Authorization;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CICTED.Controllers
+{
+    public class AccountController : Controller
+    {
+        private SignInManager<ApplicationUser> _signInManager;
+        private UserManager<ApplicationUser> _userManager;
+
+        public AccountController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpGet("login")]
+        [AllowAnonymous]
+        public IActionResult login()
+        {
+
+        }
+
+    }
+}
