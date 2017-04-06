55a5a81bffe997d68b1c1fc680479590855291b0
Bruna Silva Campos <bruna.sc1997@hotmail.com>
2017-04-06 15:14:00 -0300

    Add Model in ViewModel

--- CICTED/src/CICTED.Domain/ViewModels/Account/LoginViewModel.cs
+++ CICTED/src/CICTED.Domain/ViewModels/Account/LoginViewModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CICTED.Domain.Infrastucture
+namespace CICTED.Domain.ViewModels.Account
 {
     public class LoginViewModel
     {
