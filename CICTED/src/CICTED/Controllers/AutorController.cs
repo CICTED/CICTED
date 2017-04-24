1741ffab4efa17d034a040f509aaeaae2d42feaf
Bruna Silva Campos <bruna.sc1997@hotmail.com>
2017-04-24 18:40:28 -0300

    page home autor

--- CICTED/src/CICTED/Controllers/AutorController.cs
+++ CICTED/src/CICTED/Controllers/AutorController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using CICTED.Domain.Entities.Account;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,9 +13,17 @@ namespace CICTED.Controllers
     [Route("autor")]
     public class AutorController : Controller
     {
-        [HttpGet("home")]
-        public IActionResult Home()
+        private UserManager<ApplicationUser> _userManager;
+
+        public AutorController(UserManager<ApplicationUser> userManager)
         {
+            _userManager = userManager;
+        }
+        [HttpGet("home")]
+        public async Task<IActionResult> Home()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            //ViewBag.Nome = user.Nome;
             return View();
         }
     }
