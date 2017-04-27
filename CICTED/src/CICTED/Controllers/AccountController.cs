adea0fa36cf71812078e07e7ffe687caec7dd860
LuizGustavoPereira <pereiraguus@gmail.com>
2017-04-27 18:06:06 -0300

    ...

--- CICTED/src/CICTED/Controllers/AccountController.cs
+++ CICTED/src/CICTED/Controllers/AccountController.cs
@@ -238,6 +238,12 @@ namespace CICTED.Controllers
             {
                 var user = await _userManager.FindByNameAsync(User.Identity.Name);
 
+                if (validarCPF(model.CPF) == false)
+                {
+                    ViewBag.CPF = true;
+                    return RedirectToAction("Registrar");
+                }
+
                 long cidadeId = 0;
                 long enderecoId = 0;
                 var endereco = new Endereco
@@ -267,9 +273,7 @@ namespace CICTED.Controllers
                 {
                     cidadeId = model.CidadeId;
                     enderecoId = await _localizacaoRepository.InsertEndereco(endereco, cidadeId);
-                }
-
-                
+                }               
 
                 var usuarioDados = new RegistrarViewModel()
                 {
@@ -311,5 +315,52 @@ namespace CICTED.Controllers
 
             }
         }
+
+
+        public static bool validarCPF(string CPF)
+        {
+            int[] mt1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] mt2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            string TempCPF;
+            string Digito;
+            int soma;
+            int resto;
+
+            CPF = CPF.Trim();
+            CPF = CPF.Replace(".", "").Replace("-", "");
+
+            if (CPF.Length != 11)
+                return false;
+
+            TempCPF = CPF.Substring(0, 9);
+            soma = 0;
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(TempCPF[i].ToString()) * mt1[i];
+
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            Digito = resto.ToString();
+            TempCPF = TempCPF + Digito;
+            soma = 0;
+
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(TempCPF[i].ToString()) * mt2[i];
+
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            Digito = Digito + resto.ToString();
+
+            return CPF.EndsWith(Digito);
+        }
     }
+
 }
+
