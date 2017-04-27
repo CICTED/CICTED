e3dd20e5648ffcde1286eff12b2a9b003f0c17ac
Bruna Silva Campos <bruna.sc1997@hotmail.com>
2017-04-27 16:34:38 -0300

    AccountController

--- CICTED/src/CICTED.Domain/Infrastucture/Repository/AccountRepository.cs
+++ CICTED/src/CICTED.Domain/Infrastucture/Repository/AccountRepository.cs
@@ -1,6 +1,8 @@
 ﻿
+using CICTED.Domain.Entities.Instituicao;
 using CICTED.Domain.Infrastucture.Repository.Interfaces;
 using CICTED.Domain.Models.Settings;
+using CICTED.Domain.ViewModels.Account;
 using Dapper;
 using Microsoft.Extensions.Options;
 using System;
@@ -22,5 +24,62 @@ namespace CICTED.Domain.Infrastucture.Repository
             _settings = settings.Value;
         }
         #endregion
+
+        public async Task<bool> UpdateDadosUsuario(RegistrarViewModel user, long enderecoId)
+        {
+            try
+            {
+                using (var db = new SqlConnection(_settings.ConnectionString))
+                {
+                    var updateDadosQuery = "UPDATE dbo.AspNetUsers SET Bolsista = @Bolsista, CPF = @CPF, Celular = @Celular, " +
+                        "DataNascimento = @DataNascimento, Documento = @Documento, EmailSecundario = @EmailSecundario, Estudante = @Estudante, " +
+                        "Genero = @Genero, Nome = @Nome, PhoneNumber = @PhoneNumber, Sobrenome = @Sobrenome, EnderecoId = @EnderecoId, " +
+                        "CursosId = @CursosId, InstituicaoId = @InstituicaoId, Email = @Email";
+
+                    var resultadoDados = await db.ExecuteAsync(updateDadosQuery,
+                                                                new
+                                                                {
+                                                                    Bolsista = user.Bolsista,
+                                                                    CPF = user.CPF,
+                                                                    Celular = user.Celular,
+                                                                    DataNascimento = user.DataNascimento,
+                                                                    Documento = user.Documento,
+                                                                    EmailSecundario = user.EmailSecundario,
+                                                                    Estudante = user.Estudante,
+                                                                    Genero = user.Genero,
+                                                                    Nome = user.Nome,
+                                                                    PhoneNumber = user.Telefone,
+                                                                    Sobrenome = user.Sobrenome,
+                                                                    EnderecoId = enderecoId,
+                                                                    CursosId = user.Curso,
+                                                                    InstituicaoId = user.InstituicaoId,
+                                                                    Email = user.Email
+                                                                });
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+            public async Task<List<Instituicao>> GetInstituicao()
+        {
+            try
+            {
+                using (var db = new SqlConnection(_settings.ConnectionString))
+                {
+                    var Instituicoes = await db.QueryAsync<Instituicao>("SELECT * FROM dbo.Instituicao");
+
+                    return Instituicoes.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+   
     }        
 }
--- CICTED/src/CICTED.Domain/Infrastucture/Repository/Interfaces/IAccountRepository.cs
+++ CICTED/src/CICTED.Domain/Infrastucture/Repository/Interfaces/IAccountRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CICTED.Domain.Entities.Instituicao;
+using CICTED.Domain.ViewModels.Account;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +9,7 @@ namespace CICTED.Domain.Infrastucture.Repository.Interfaces
 {
     public interface IAccountRepository
     {
-        
+        Task<bool> UpdateDadosUsuario(RegistrarViewModel user, long enderecoId);
+        Task<List<Instituicao>> GetInstituicao();
     }
 }
