a3ef57b714554d63ff98b1b6d42a5aa1328531e8
lamnaldi <laramenaldi@gmail.com>
2017-05-03 13:01:09 -0300

    ...

--- CICTED/src/CICTED/Controllers/AdministradorController.cs
+++ CICTED/src/CICTED/Controllers/AdministradorController.cs
@@ -8,20 +8,18 @@ using CICTED.Domain.Entities.Account;
 using Microsoft.AspNetCore.Authorization;
 using CICTED.Domain.Infrastucture.Repository;
 using CICTED.Domain.Infrastucture.Repository.Interfaces;
-using CICTED.Domain.ViewModels.Autor;
-using CICTED.Domain.ViewModels.Trabalho;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace CICTED.Controllers
 {
-    [Route("autor")]
-    public class AutorController : Controller
+    [Route("administrador")]
+    public class AdministradorController : Controller
     {
         private UserManager<ApplicationUser> _userManager;
         private IAccountRepository _accountRepository;
 
-        public AutorController(UserManager<ApplicationUser> userManager, IAccountRepository accountRepository)
+        public AdministradorController(UserManager<ApplicationUser> userManager, IAccountRepository accountRepository)
         {
             _userManager = userManager;
             _accountRepository = accountRepository;
@@ -29,30 +27,12 @@ namespace CICTED.Controllers
         [HttpGet("home")]
         [Authorize]
         public async Task<IActionResult> Home()
-        {           
+        {
             var user = await _userManager.FindByNameAsync(User.Identity.Name);
             var roles = await _accountRepository.GetRoles(user.Id);
-            var model = new MenuViewModel();
-            model.Roles = roles;
             ViewBag.Nome = user.Nome;
-            return View(model);
+            return View();
         }
-
-        [HttpGet("CadastroTrabalho")]
-        public IActionResult cadastroTrabalho()
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(); 
-            }
-
-            CadastroTrabalhoViewModel model = new CadastroTrabalhoViewModel();
-
-            return View(model);
-        }
-
-
-
     }
 
 }
