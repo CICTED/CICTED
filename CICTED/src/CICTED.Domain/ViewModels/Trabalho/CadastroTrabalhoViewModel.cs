8e6a61313b15357a26cca82be95ee50b240cef02
LuizGustavoPereira <pereiraguus@gmail.com>
2017-05-03 12:56:58 -0300

    ...

---
+++ CICTED/src/CICTED.Domain/ViewModels/Trabalho/CadastroTrabalhoViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CICTED.Domain.Entities.Localizacao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CICTED.Domain.ViewModels.Trabalho
+{
+    public class CadastroTrabalhoViewModel
+    {
+        public string Evento { get; set; }
+
+        public int AreaConhecimento { get; set; }
+
+        public int SubArea { get; set; }
+
+        public int PeriodoApresentacao { get; set; }
+
+        public bool TabalhoFinanciado { get; set; }
+
+        public int Agencia { get; set; }
+
+        public string TextoCitacao { get; set; }
+
+        public string CodigoCEP { get; set; }
+
+        public List<Cidade> Cidades { get; set; }
+
+        public List<Estado> Estado { get; set; }
+
+    }
+}
