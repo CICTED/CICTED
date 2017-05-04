a9d421056c66604a69d07de2d972779e3fe9191a
LuizGustavoPereira <pereiraguus@gmail.com>
2017-05-04 14:24:05 -0300

    ...

--- CICTED/src/CICTED/wwwroot/_references.js
+++ CICTED/src/CICTED/wwwroot/_references.js
@@ -1,6 +1,6 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="js/cicted.min.js" />
-/// <reference path="js/site - copy.js" />
+/// <reference path="js/jquery.maskedinput.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
