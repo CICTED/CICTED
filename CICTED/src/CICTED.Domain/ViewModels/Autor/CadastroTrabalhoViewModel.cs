bfb63a1d5475f889fdedca305e277834fb35012d
LuizGustavoPereira <pereiraguus@gmail.com>
2017-05-04 15:22:39 -0300

    ...

--- CICTED/src/CICTED.Domain/ViewModels/Autor/CadastroTrabalhoViewModel.cs
+++ CICTED/src/CICTED.Domain/ViewModels/Autor/CadastroTrabalhoViewModel.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CICTED.Domain.ViewModels.Trabalho
+namespace CICTED.Domain.ViewModels.Autor
 {
     public class CadastroTrabalhoViewModel
     {
---
+++ CICTED/src/CICTED.Domain/ViewModels/Autor/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CICTED.Domain.ViewModels.Autor
+{
+    public class HomeViewModel
+    {
+        public string EventoNome { get; set; }
+        public string Descricao { get; set; }
+        public string PublicoAlvo { get; set; }
+    }
+}
