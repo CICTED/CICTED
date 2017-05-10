bd490c61562231b13081956cbfe0aff4a533f023
lamnaldi <laramenaldi@gmail.com>
2017-05-10 17:31:39 -0300

    ...

--- CICTED/src/CICTED.Domain/Infrastucture/Repository/Interfaces/ILocalizacaoRepository.cs
+++ CICTED/src/CICTED.Domain/Infrastucture/Repository/Interfaces/ILocalizacaoRepository.cs
@@ -9,9 +9,10 @@ namespace CICTED.Domain.Infrastucture.Repository.Interfaces
 {
     public interface ILocalizacaoRepository
     {
-        Task<List<Estado>> GetEstado();
+        Task<List<Estado>> GetEstados();
+        Task<List<Estado>> GetEstado(long cidadeId);
         Task<List<Cidade>> GetCidades(int estadoId);
         Task<long> InsertEnderecoExterior(EnderecoExterior endereco);
         Task<long> InsertEndereco(Endereco endereco, long cidadeId, long enderecoExterior = 0);
-     }
+    }
 }
