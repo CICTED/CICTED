8b445b0559057da378eb089b537fbabef61ac1de
lamnaldi <laramenaldi@gmail.com>
2017-06-12 18:29:23 -0300

    ...

--- CICTED/src/CICTED/Controllers/AdministradorController.cs
+++ CICTED/src/CICTED/Controllers/AdministradorController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Identity;
 using CICTED.Domain.Entities.Account;
 using Microsoft.AspNetCore.Authorization;
 using CICTED.Domain.Infrastucture.Repository.Interfaces;
+using CICTED.Domain.ViewModels.Administrador;
+using System.Collections.Generic;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -20,9 +22,10 @@ namespace CICTED.Controllers
         private IAreaRepository _areaRepository;
         private IAutorRepository _autorRepository;
         private IAgenciaRepository _agenciaRepository;
+        private IAdministradorRepository _administradorRepository;
 
 
-        public AdministradorController(ITrabalhoRepository trabalhoRepository, UserManager<ApplicationUser> userManager, IAccountRepository accountRepository, IEventoRepository eventoRepository, IAreaRepository areaRepository, IAutorRepository autorRepository, IAgenciaRepository agenciaRepository)
+        public AdministradorController(ITrabalhoRepository trabalhoRepository, UserManager<ApplicationUser> userManager, IAccountRepository accountRepository, IEventoRepository eventoRepository, IAreaRepository areaRepository, IAutorRepository autorRepository, IAgenciaRepository agenciaRepository, IAdministradorRepository administradorRepository)
         {
             _trabalhoRepository = trabalhoRepository;
             _userManager = userManager;
@@ -31,13 +34,38 @@ namespace CICTED.Controllers
             _areaRepository = areaRepository;
             _autorRepository = autorRepository;
             _agenciaRepository = agenciaRepository;
+            _administradorRepository = administradorRepository;
         }
 
         [HttpGet("gerenciarOrganizador")]
         [Authorize]
         public async Task<IActionResult> GerenciarOrganizador()
         {
-            return View();
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var organizadores = await _administradorRepository.GetOrganizador();
+            List<GerenciarOrganizador> model = new List<GerenciarOrganizador>();
+
+            foreach (var organizador in organizadores)
+            {
+
+                var organizadorConsulta = new GerenciarOrganizador()
+                {
+                    Id = organizador.Id,
+                    Nome = organizador.Nome,
+                    Sobrenome = organizador.Sobrenome,
+                    Telefone = organizador.Telefone,
+                    Nascimento = organizador.Nascimento,
+                    Email = organizador.Email,
+                    Genero = organizador.Genero,
+                    Celular = organizador.Celular,
+                    CPF = organizador.CPF, 
+                    Avaliador = organizador.Avaliador
+                };
+                model.Add(organizadorConsulta);
+            }
+
+            return View(model);
+            
         }
     }
 
