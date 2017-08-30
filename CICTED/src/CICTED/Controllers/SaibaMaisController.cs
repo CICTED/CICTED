67e4f1efd4ec9e976df6e11b1c9b0b9994a3b2e3
lamnaldi <laramenaldi@gmail.com>
2017-08-30 17:57:01 -0300

    ...

---
+++ CICTED/src/CICTED/Controllers/SaibaMaisController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CICTED.Controllers
+{
+    [Route("saibaMais")]
+    public class SaibaMaisController : Controller
+    {
+        [HttpGet("home")]
+        public async Task<IActionResult> Home()
+        {
+            return View();
+        }
+    }
+}
