47c7850598fbc7b6556c04b32f1c8e4dc9996328
nathysq <nathysq@users.noreply.github.com>
2017-09-12 14:18:15 -0300

    Primeiro commit - Nathália

---
+++ CICTED/src/CICTED/Controllers/MessageController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CICTED.Controllers
+{
+    public class MessageController : Controller
+    {
+        
+    }
+}
