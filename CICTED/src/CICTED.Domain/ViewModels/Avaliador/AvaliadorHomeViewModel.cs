ac4c956905923d2d02db9261692f83e97b605d91
Bruna Silva Campos <bruna.sc1997@hotmail.com>
2017-09-15 15:35:42 -0300

    Criação da pagina inicial de Avaliadores

---
+++ CICTED/src/CICTED.Domain/ViewModels/Avaliador/AvaliadorHomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CICTED.Domain.ViewModels.Avaliador
+{
+    class AvaliadorHomeViewModel
+    {
+        public int Pendentes { get; set; }
+        public int Avaliados { get; set; }
+        public int Relevantes { get; set; }
+        public int Aprovados { get; set; }
+        public int Reprovados { get; set; }
+    }
+}
--- CICTED/src/CICTED/wwwroot/js/CICTED.js
+++ CICTED/src/CICTED/wwwroot/js/CICTED.js
@@ -25,11 +25,11 @@
     };
 
     $.fn.subArea = function (dropSubAreas) {
-        var dropDown = $(this);
-        var subAreaId = dropSubAreas.val();
+        var dropDown = $(this);        
 
         dropDown.on('change', function () {
             var areaId = $(this).val();           
+            dropSubAreas.html('<option value="-1">Selecione uma area</option>');
 
                 $.ajax({
                     url: '/trabalho/list/subarea/' + areaId,
@@ -48,44 +48,6 @@
                     }
                 });
         });
-    };
-         
-    //$.fn.subAreas = function (dropSubAreas) {
-    //    var dropDown = $(this);
-    //    var subAreaId = dropSubAreas.val();
-
-    //    dropDown.on('change', function () {
-    //        var areaId = $(this).val();
-
-           
-    //            $.ajax({
-    //                url: '/administrador/list/subarea/' + areaId,
-    //                method: 'GET',
-    //                success: function (data) {
-    //                    console.log(data);
-    //                    if (areaId == 1) {
-    //                        dropSubAreas.append('<optgroup label="Ciencias Biologicas"></optgroup>')
-    //                    } else {
-    //                        if (areaId == 2) {
-    //                            dropSubAreas.append('<optgroup label="Ciencias Exatas"></optgroup>')
-    //                        } else {
-    //                            if (areaId == 3) {
-    //                                dropSubAreas.append('<optgroup label="Ciencias Humanas"></optgroup>')
-    //                            }
-    //                        }
-    //                    }
-    //                    $.each(data, function (i, item) {
-    //                        dropSubAreas.append('<option value="' + item.id + '">' + item.nome + '</option>');
-
-    //                    });
-    //                    dropSubAreas.removeAttr('disabled');
-    //                },
-    //                error: function (x, y, message) {
-    //                    console.log(data);
-    //                    dropSubAreas.attr('disabled', 'disabled');
-    //                }
-    //            });
-    //    });
-    //};
+    };             
 
 })(jQuery);
